b06fb250c0b31e84fd6608ded90781265e95dee6
Mateus Damasceno <mateus.damasceno@gmail.com>
2024-08-07 15:43:08 -0300

    configuracao mysql

---
+++ src/main/java/com/didatikos/estoque/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.didatikos.estoque.repository;
+
+
+import com.didatikos.estoque.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Product, Long> {
+}
